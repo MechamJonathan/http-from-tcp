7871fa1dda73c6f585acfbf0d1b320429ef7dd59
MechamJonathan <jonsmec95@gmail.com>
2025-05-17 08:05:39 -0600

    extracted getLinesChannel function

--- main.go
+++ main.go
@@ -11,6 +11,46 @@ import (
 
 const inputFilePath = "messages.txt"
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	linesChan := make(chan string)
+
+	go func() {
+		defer f.Close()
+
+		var currentLine string
+		for {
+			buf := make([]byte, 8)
+			n, err := f.Read(buf)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if currentLine != "" {
+						linesChan <- currentLine
+					}
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+
+			parts := strings.Split(string(buf[:n]), "\n")
+
+			for i, part := range parts {
+				if i == len(parts)-1 {
+					currentLine += part
+				} else {
+					completeLine := currentLine + part
+					linesChan <- completeLine
+					currentLine = ""
+				}
+			}
+		}
+
+		close(linesChan)
+	}()
+
+	return linesChan
+}
+
 func main() {
 	file, err := os.Open(inputFilePath)
 	if err != nil {
@@ -21,31 +61,8 @@ func main() {
 	fmt.Printf("Reading data from: %s\n", inputFilePath)
 	fmt.Println("------------------------------------")
 
-	var currentLine string
-	for {
-		buf := make([]byte, 8)
-		n, err := file.Read(buf)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				if currentLine != "" {
-					fmt.Printf("read: %s\n", currentLine)
-				}
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-
-		parts := strings.Split(string(buf[:n]), "\n")
-
-		for i, part := range parts {
-			if i == len(parts)-1 {
-				currentLine += part
-			} else {
-				completeLine := currentLine + part
-				fmt.Printf("read: %s\n", completeLine)
-				currentLine = ""
-			}
-		}
+	linesChan := getLinesChannel(file)
+	for line := range linesChan {
+		fmt.Printf("read: %s\n", line)
 	}
 }
