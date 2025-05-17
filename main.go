5ffa8838944a5fca6064aea970b3df0450c83f34
MechamJonathan <jonsmec95@gmail.com>
2025-05-17 07:54:59 -0600

    print by new lines

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -20,17 +21,31 @@ func main() {
 	fmt.Printf("Reading data from: %s\n", inputFilePath)
 	fmt.Println("------------------------------------")
 
+	var currentLine string
 	for {
 		buf := make([]byte, 8)
 		n, err := file.Read(buf)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
+				if currentLine != "" {
+					fmt.Printf("read: %s\n", currentLine)
+				}
 				break
 			}
 			fmt.Printf("error: %s\n", err.Error())
 			break
 		}
 
-		fmt.Printf("read: %s\n", string(buf[:n]))
+		parts := strings.Split(string(buf[:n]), "\n")
+
+		for i, part := range parts {
+			if i == len(parts)-1 {
+				currentLine += part
+			} else {
+				completeLine := currentLine + part
+				fmt.Printf("read: %s\n", completeLine)
+				currentLine = ""
+			}
+		}
 	}
 }
